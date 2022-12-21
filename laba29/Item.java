edb91b8a2bff01cf18ff98a78035e64894756438
Agcon <90468059+Agcon@users.noreply.github.com>
2022-12-21 16:47:46 +0300

    Delete laba29 directory

--- laba29/Item.java
+++
@@ -1,9 +0,0 @@
-package ru.agcon.new_labs.laba29;
-
-public interface Item{
-    public int getCost();
-    public String getName();
-    public String getDiscription();
-
-
-}
--- laba29/OrderAlreadyException.java
+++
@@ -1,7 +0,0 @@
-package ru.agcon.new_labs.laba29;
-
-public class OrderAlreadyException extends Exception{
-    public OrderAlreadyException(String adress){
-        super("The order for " + adress + " is added!");
-    }
-}
